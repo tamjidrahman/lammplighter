7723697473c92f5cdea305a4100f756b7cd5be54
Tamjid Rahman <tamjidarrahman@gmail.com>
2023-11-05 00:29:13 -0400

    add db user

--- api/database/database.py
+++ api/database/database.py
@@ -1,7 +1,8 @@
 import os
 
+import boto3
 from dotenv import load_dotenv
-from sqlalchemy import create_engine
+from sqlalchemy import URL, create_engine
 from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
@@ -10,9 +11,28 @@ DB_ENGINE = os.environ.get("DB_ENGINE")
 DB_USER = os.environ.get("DB_USER")
 DB_HOSTNAME = os.environ.get("DB_HOSTNAME")
 DB_DATABASE = os.environ.get("DB_DATABASE")
+DB_PORT = os.environ.get("DB_PORT")
 
-DB_URL = f"{DB_ENGINE}://{DB_USER}@{DB_HOSTNAME}/{DB_DATABASE}"
+if "rds" in DB_HOSTNAME:
+    session = boto3.Session()
+    client = session.client("rds")
+    password = client.generate_db_auth_token(
+        DBHostname=DB_HOSTNAME,
+        Port=DB_PORT,
+        DBUsername=DB_USER,
+        Region="us-east-2",
+    )
+else:
+    password = ""
+
+url_object = URL.create(
+    DB_ENGINE,
+    username=DB_USER,
+    password=password,
+    host=DB_HOSTNAME,
+    database=DB_DATABASE,
+)
 
 
-engine = create_engine(DB_URL)
+engine = create_engine(url_object)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
