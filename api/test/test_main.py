d0e818bf798f2cfc79e9c2085ce4a3afcc5162bf
Tamjid Rahman <tamjidarrahman@gmail.com>
2023-11-05 16:03:38 -0500

    fix test

--- api/test/test_main.py
+++ api/test/test_main.py
@@ -1,5 +1,3 @@
-from unittest.mock import patch
-
 from fastapi.testclient import TestClient
 
 from api.main import app
@@ -7,8 +5,7 @@ from api.main import app
 client = TestClient(app)
 
 
-@patch("api.main.lammps", __version__=0)
-def test_version(mock_lammps):
+def test_version():
     response = client.get("/healthcheck")
     assert response.status_code == 200
-    assert response.json() == {"lammps_version": mock_lammps.__version__}
+    assert response.json() == {"lammps_version": 0}
